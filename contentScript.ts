d162ce0342e42c9fba3f4e2a5a144449deacede1
Lex Pan <lexiang.pan@gmail.com>
2024-12-07 14:51:32 -0500

    manifest.json creation

---
+++ contentScript.ts
@@ -0,0 +1,2 @@
+// boAvmrAFfwZEHebYjctgTppwiEwazqIftEMU             job title class identifier 
+// job-card-container__footer-item job-card-container__footer-job-state t-bold      viewed job class identifier 
